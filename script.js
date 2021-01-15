bbdb2bbc9e10c1a3d0552f79a00ec80abf3dc8b2
pankaj485 <pankaj485@users.noreply.github.com>
2021-01-15 20:52:57 +0530

    version 2
    added delete feature , task turns yellow on clicking checkbox

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ document.querySelector('.addButton').addEventListener('click', (event) => {
 	div.classList.add('newItems');
 
 	let addCheckbox = document.createElement('input');
+	addCheckbox.name = 'checkBox';
 	addCheckbox.type = 'checkbox';
 	addCheckbox.classList.add('updateState');
+
+	let image = document.createElement('img');
+	image.classList.add('deleteIcon');
+	image.src = './delete.svg';
 
 	if (p.innerText.length === 0) {
 		alert('NO Task ADDED !');
 	} else {
+		div.appendChild(image);
 		div.appendChild(addCheckbox);
 		div.appendChild(p);
 		document.querySelector('.listItems').appendChild(div);
 		document.querySelector('#newItem').value = '';
 	}
+
+	image.addEventListener('click', () => {
+		div.parentNode.removeChild(div);
+		// div.classList.add('removeNow');
+		console.log('removed');
+	});
 });
